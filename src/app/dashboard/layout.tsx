8c6417de2a19a76e5b3814d28fcf775335ee6655
AhmedReda958 <areda.gad@gmail.com>
2024-08-04 01:32:58 +0300

    server details

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,10 +7,8 @@ function Dashboard({ children }: { children: React.ReactNode }) {
       <Sidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <Header />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
-            {children}
-          </div>
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+          {children}
         </main>
       </div>
     </div>
--- src/app/dashboard/servers/page.tsx
+++ src/app/dashboard/servers/page.tsx
@@ -2,50 +2,53 @@ import { LuServer, LuServerOff } from "react-icons/lu";
 import { FaPlus } from "react-icons/fa";
 import Link from "next/link";
 import data from "@/data";
+import { Card, CardContent } from "@/components/ui/card";
 
 const ServerCard: React.FC<{ server: (typeof data)[0] }> = ({ server }) => {
   return (
     <Link href={`servers/${server.id}`}>
-      <div className="p-4 bg-white dark:bg-primary shadow-md rounded-lg flex gap-6">
-        <div className="flex-shrink-0">
-          {server.online ? (
-            <LuServer className="w-16 h-16 text-green-600 dark:text-green-400 " />
-          ) : (
-            <LuServerOff className="w-16 h-16 text-gray-600" />
-          )}
-        </div>
-        <div className="flex-1">
-          <h3 className="text-lg font-semibold">Server {server.id}</h3>
-          <p className="text-sm">IP: {server.addresses[0].ip}</p>
-          <p className="text-sm">
-            CPU:{" "}
-            <span
-              className={server.cpu_pc >= 80 ? "text-red-400 font-bold" : ""}
-            >
-              {server.cpu_pc}%
-            </span>
-          </p>
-          <p className="text-sm ">
-            Status:{" "}
+      <Card>
+        <CardContent className="p-4  shadow-md rounded-lg flex gap-6">
+          <div className="flex-shrink-0">
             {server.online ? (
-              <span className="font-bold text-green-500 dark:text-green-400">
-                Online
-              </span>
+              <LuServer className="w-16 h-16 text-green-600 dark:text-green-400 " />
             ) : (
-              <span className="font-bold text-red-500 dark:text-red-400">
-                Offline
-              </span>
+              <LuServerOff className="w-16 h-16 text-gray-600" />
             )}
-          </p>
-        </div>
-      </div>
+          </div>
+          <div className="flex-1 ">
+            <h3 className="text-lg font-semibold">Server {server.id}</h3>
+            <p className="text-sm">IP: {server.addresses[0].ip}</p>
+            <p className="text-sm">
+              CPU:{" "}
+              <span
+                className={server.cpu_pc >= 80 ? "text-red-400 font-bold" : ""}
+              >
+                {server.cpu_pc}%
+              </span>
+            </p>
+            <p className="text-sm ">
+              Status:{" "}
+              {server.online ? (
+                <span className="font-bold text-green-500 dark:text-green-400">
+                  Online
+                </span>
+              ) : (
+                <span className="font-bold text-red-500 dark:text-red-400">
+                  Offline
+                </span>
+              )}
+            </p>
+          </div>
+        </CardContent>
+      </Card>
     </Link>
   );
 };
 
 const Servers: React.FC = () => {
   return (
-    <div>
+    <div className="col-span-full">
       <h2 className="mb-6">Servers</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {data.map((server) => (
