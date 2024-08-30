47a709bb6b5ebb4cb1f905e7b8d51031e989a4f0
AhmedReda958 <areda.gad@gmail.com>
2024-08-30 14:12:53 +0300

    refactor dashboard sidebar

--- src/app/dashboard/_components/Sidebar.tsx
+++ src/app/dashboard/_components/Sidebar.tsx
@@ -19,119 +19,142 @@ import {
 } from "@/components/ui/tooltip";
 import { usePathname } from "next/navigation";
 import { BiSupport } from "react-icons/bi";
+import { useSession } from "next-auth/react";
+
+type SidebarItem = {
+  icon: React.ElementType;
+  name: string;
+  href: string;
+  role: string;
+  position: string;
+};
+
+const sidebarItems: SidebarItem[] = [
+  {
+    icon: LuHome,
+    name: "Dashboard",
+    href: "/dashboard",
+    role: "admin",
+    position: "top",
+  },
+  {
+    icon: LuServer,
+    name: "Servers",
+    href: "/dashboard/servers",
+    role: "all",
+    position: "top",
+  },
+  {
+    icon: LuUsers2,
+    name: "Users",
+    href: "/dashboard/users",
+    role: "admin",
+    position: "top",
+  },
+  {
+    icon: LuLineChart,
+    name: "Analytics",
+    href: "/dashboard/analytics",
+    role: "admin",
+    position: "top",
+  },
+  {
+    icon: LiaFileInvoiceDollarSolid,
+    name: "Invoices",
+    href: "/dashboard/invoices",
+    role: "user",
+    position: "top",
+  },
+  {
+    icon: LuPackage,
+    name: "Products",
+    href: "/dashboard/products",
+    role: "all",
+    position: "top",
+  },
+  {
+    icon: BiSupport,
+    name: "Support",
+    href: "/dashboard/support",
+    role: "user",
+    position: "bottom",
+  },
+  {
+    icon: LuSettings,
+    name: "Settings",
+    href: "/dashboard/settings",
+    role: "all",
+    position: "bottom",
+  },
+];
+
+const SidebarItem = ({ item }: { item: SidebarItem }) => {
+  const pathname = usePathname();
+  const session = useSession();
+
+  // return admin items if user is admin and role is admin
+  // return user items if user is user and role is user
+  // return all items if role is all
+  console.log(session.data?.user);
+  if (session.data?.user.role == item.role || item.role === "all") {
+    return (
+      <Tooltip key={item.href}>
+        <TooltipTrigger asChild>
+          <Link
+            href={item.href}
+            className={`flex h-9 w-9 items-center justify-center rounded-lg ${
+              pathname === item.href
+                ? "bg-accent text-accent-foreground"
+                : "text-muted-foreground"
+            } transition-colors hover:text-foreground md:h-8 md:w-8`}
+          >
+            <item.icon className="h-5 w-5" />
+            <span className="sr-only">{item.name}</span>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent side="right">{item.name}</TooltipContent>
+      </Tooltip>
+    );
+  }
+  return null;
+};
 
 const Sidebar = () => {
-  const pathname = usePathname();
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-      <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-        <TooltipProvider>
-          <Link
-            href="https://splexhosting.com/"
-            className="h-9 w-9 text-center"
-          >
+      <TooltipProvider>
+        <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+          <Link href="/" className="h-9 w-9 text-center">
             <Image
               src={"/favicon.ico"}
-              alt="Splexhosting"
+              alt="VenixHosting"
               width={32}
               height={32}
               className="ps-1 hover:scale-110 transition-transform"
             />
-            <span className="sr-only">SplexHosting</span>
+            <span className="sr-only">VenixHosting</span>
           </Link>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="/dashboard/servers"
-                className={`flex h-9 w-9 items-center justify-center rounded-lg ${
-                  pathname === "/dashboard/servers"
-                    ? "bg-accent text-accent-foreground"
-                    : "text-muted-foreground"
-                } transition-colors hover:text-foreground md:h-8 md:w-8`}
-              >
-                <LuServer className="h-5 w-5" />
-                <span className="sr-only">Servers</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Servers</TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className={`flex h-9 w-9 items-center justify-center rounded-lg ${
-                  pathname === "/dashboard/invoices"
-                    ? "bg-accent text-accent-foreground"
-                    : "text-muted-foreground"
-                } transition-colors hover:text-foreground md:h-8 md:w-8`}
-              >
-                <LiaFileInvoiceDollarSolid className="h-5 w-5" />
-                <span className="sr-only">Invoices</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Invoices</TooltipContent>
-          </Tooltip>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className={`flex h-9 w-9 items-center justify-center rounded-lg ${
-                  pathname === "/dashboard/products"
-                    ? "bg-accent text-accent-foreground"
-                    : "text-muted-foreground"
-                } transition-colors hover:text-foreground md:h-8 md:w-8`}
-              >
-                <LuPackage className="h-5 w-5" />
-                <span className="sr-only">Products</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Products</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </nav>
-      <div className="flex-1 flex items-center justify-center">
-        <span className="-rotate-90 origin-center text-xl text-muted-foreground tracking-widest select-none opacity-60">
-          VenixHosting
-        </span>
-      </div>
-      <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className={`flex h-9 w-9 items-center justify-center rounded-lg ${
-                  pathname === "/dashboard/support"
-                    ? "bg-accent text-accent-foreground"
-                    : "text-muted-foreground"
-                } transition-colors hover:text-foreground md:h-8 md:w-8`}
-              >
-                <BiSupport className="h-5 w-5" />
-                <span className="sr-only">Support</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Support</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className={`flex h-9 w-9 items-center justify-center rounded-lg ${
-                  pathname === "/dashboard/settings"
-                    ? "bg-accent text-accent-foreground"
-                    : "text-muted-foreground"
-                } transition-colors hover:text-foreground md:h-8 md:w-8`}
-              >
-                <LuSettings className="h-5 w-5" />
-                <span className="sr-only">Settings</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Settings</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </nav>
+          {sidebarItems
+            .filter((item) => item.position === "top")
+            .map((item) => (
+              <SidebarItem key={item.href} item={item} />
+            ))}
+        </nav>
+        <div className="flex-1 flex items-center justify-center">
+          <span className="-rotate-90 origin-center text-xl text-muted-foreground tracking-widest select-none opacity-60">
+            VenixHosting
+          </span>
+        </div>
+
+        {/* bottom items */}
+        <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+          {sidebarItems
+            .filter((item) => item.position === "bottom")
+            .map((item) => (
+              <SidebarItem key={item.href} item={item} />
+            ))}
+        </nav>
+      </TooltipProvider>
     </aside>
   );
 };
