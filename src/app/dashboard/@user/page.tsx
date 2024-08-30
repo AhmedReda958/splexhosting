b8706f64e64ea1b5f14cd9d4cf06ff1b6f84f310
AhmedReda958 <areda.gad@gmail.com>
2024-08-30 12:07:22 +0300

    change dashboard structure to use paraller routes

--- src/app/dashboard/@user/page.tsx
+++ src/app/dashboard/@user/page.tsx
@@ -1,9 +1,10 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 const HomePage = async () => {
   const session = await getServerSession(authOptions);
-
+  redirect("/dashboard/servers");
   return (
     <div>
       <h1 className="">Wellcome {session?.user.firstName}</h1>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,20 +2,30 @@ import Header from "./_components/Header";
 import Sidebar from "./_components/Sidebar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 
-async function Dashboard({ children }: { children: React.ReactNode }) {
+async function Dashboard({
+  children,
+  admin,
+  user,
+}: {
+  children: React.ReactNode;
+  admin: React.ReactNode;
+  user: React.ReactNode;
+}) {
   const session = await getServerSession(authOptions);
-  if (session?.user?.role == "admin") {
-    redirect("/admin-dashboard");
-  }
+  // if (session?.user?.role == "admin" && usePathname() == "/dashboard") {
+  //   redirect("/admin-dashboard");
+  // } else if (session?.user?.role == "user") {
+  //   redirect("/dashboard");
+  // }
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <Sidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <Header />
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
-          {children}
+          {session?.user?.role == "admin" ? admin : user}
         </main>
       </div>
     </div>
