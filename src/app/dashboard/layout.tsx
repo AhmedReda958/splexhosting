b04e577c3ff99ec229702a646fcc775401277a3d
AhmedReda958 <areda.gad@gmail.com>
2024-09-02 20:32:35 +0300

    new server info page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -18,7 +18,7 @@ async function Dashboard({
       <Sidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <Header />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+        <main className="p-4 sm:px-6 sm:py-0">
           {session?.user?.role == "admin" ? admin : user}
         </main>
       </div>
