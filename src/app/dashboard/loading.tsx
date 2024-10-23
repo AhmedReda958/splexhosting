4d4c9b36f77b4d254c0a6e882268ae38175e2980
AhmedReda958 <areda.gad@gmail.com>
2024-10-23 13:12:55 +0300

    fix loading img

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -4,14 +4,7 @@ import Image from "next/image";
 const loading = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <Image
-        src="/imgs/big-data.gif"
-        alt="loading"
-        layout="fill"
-        objectFit="cover"
-        width={250}
-        height={250}
-      />
+      <Image src="/imgs/big-data.gif" alt="loading" width={250} height={250} />
     </div>
   );
 };
